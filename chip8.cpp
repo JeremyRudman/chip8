31bb287f511fe873c356c0abf41cbe55e4d2960a
jeremyrudman <jeremyrudman@gmail.com>
2019-11-24 01:52:28 -0500

    done with nearly all opcodes. now just need to implement everything else

--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unistd.h>
-
+#include <iostream>
+#include <fstream>
+//https://en.wikipedia.org/wiki/CHIP-8#Opcode_table used to fill opcodes
 unsigned short opcode;
 unsigned char memory[4096];
 unsigned char v[16];
@@ -12,6 +14,7 @@ unsigned char sound_timer;
 unsigned short stack[16];
 unsigned short sp;
 unsigned char key[16];
+bool drawFlag;
 
 unsigned char chip8_fontset[80] =
         {
@@ -65,27 +68,46 @@ void initialize() {
     }
 }
 
-void loadgame(char* game){ //TODO file input
-    int bufferSize;
-    unsigned char buffer[0];
-    for(int i = 0; i < bufferSize; ++i)
+void loadgame(char* game){
+    std::ifstream gameFile( game, std::ios::binary | std::ios::ate);
+    if(!gameFile) {
+        std::cout << "Cannot open file!" << std::endl;
+        return;
+    }
+    int bufferSize = gameFile.tellg();
+    gameFile.close();
+
+
+    gameFile.open(game, std::ios::binary | std::ios::in);
+    char buffer[bufferSize];
+    if(!gameFile.read(buffer,bufferSize)) {
+        std::cout << "Cannot read from file!" << std::endl;
+        return;
+    }
+    for(int i = 0; i < bufferSize; ++i){
         memory[i + 512] = buffer[i];
+    }
+    gameFile.close();
+
 }
 
 void emulateCycle() {
 
     opcode = memory[pc] << 8 | memory[pc + 1];
 
-    switch (opcode & 0xF000) {
+    switch (opcode & 0xF000) { //TODO 0x0NNN
         case 0x0000:
             switch (opcode & 0x000F) {
                 case 0x0000:
                     for(unsigned char & i :gfx){
                         i=0;
                     }
+                    pc+=2;
                     break;
                 case 0x000E:
-                    //TODO return from a subroutine
+                    pc = stack[sp] + 2;
+                    stack[sp]=0;
+                    --sp;
                     break;
                 default:
                     printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
@@ -145,47 +167,89 @@ void emulateCycle() {
         }
         case 0x8000:
             switch (opcode & 0x000F) {
-                case 0x0001: {
+                case 0x0000: {
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     unsigned short y = (0x00F0 & opcode) >> 4;
                     v[x] = v[y];
                     pc += 2;
                     break;
                 }
-                case 0x0002:{
+                case 0x0001:{
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     unsigned short y = (0x00F0 & opcode) >> 4;
                     v[x] = v[y] | v[x];
                     pc += 2;
                     break;
                 }
-                case 0x0003:{
+                case 0x0002:{
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     unsigned short y = (0x00F0 & opcode) >> 4;
                     v[x] = v[y] & v[x];
                     pc += 2;
                     break;
                 }
-                case 0x0004:{
+                case 0x0003:{
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     unsigned short y = (0x00F0 & opcode) >> 4;
                     v[x] = v[y] ^ v[x];
                     pc += 2;
                     break;
                 }
-                case 0x0005:
+                case 0x0004: {
+                    unsigned short x = (0x0F00 & opcode) >> 8;
+                    unsigned short y = (0x00F0 & opcode) >> 4;
+                    if( v[x] + v[y] > 0xFFFF){
+                        v[15]=1;
+                    } else {
+                        v[15]=0;
+                    }
+                    v[x] = v[x] + v[y];
+                    pc+=2;
                     break;
-                case 0x0006:
+                }
+                case 0x0005:{
+                    unsigned short x = (0x0F00 & opcode) >> 8;
+                    unsigned short y = (0x00F0 & opcode) >> 4;
+                    if( v[x] - v[y] < 0x0000){
+                        v[15]=1;
+                    } else {
+                        v[15]=0;
+                    }
+                    v[x] = v[x] - v[y];
+                    pc+=2;
                     break;
-                case 0x0007:
+                }
+                case 0x0006: {
+                    unsigned short x = (0x0F00 & opcode) >> 8;
+                    v[15] = v[x] & 0x000F;
+                    v[x] = v[x] >> 1;
+                    pc+=2;
                     break;
-                case 0x000E:
+                }
+                case 0x0007:{
+                    unsigned short x = (0x0F00 & opcode) >> 8;
+                    unsigned short y = (0x00F0 & opcode) >> 4;
+                    if( v[y] - v[x] < 0x0000){
+                        v[15]=1;
+                    } else {
+                        v[15]=0;
+                    }
+                    v[x] = v[y] - v[x];
+                    pc+=2;
                     break;
+                }
+                case 0x000E:{
+                    unsigned short x = (0x0F00 & opcode) >> 8;
+                    v[15] = v[x] & 0xF000;
+                    v[x] = v[x] << 1;
+                    pc+=2;
+                    break;
+                }
                 default:
                     printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
             }
             break;
-        case 0x9000:{ //skip next instruction if V[X] != V[Y]
+        case 0x9000:{
             unsigned short x = (0x0F00 & opcode) >> 8;
             unsigned short y = (0x00F0 & opcode) >> 4;
             if (v[x] != v[y]) {
@@ -196,8 +260,9 @@ void emulateCycle() {
             break;
         }
         case 0xA000: {
-            unsigned short n = opcode & 0x0FFF;
+            unsigned short n = (opcode & 0x0FFF) << 4;
             I = n;
+            pc+=2;
             break;
         }
         case 0xB000: {
@@ -209,10 +274,30 @@ void emulateCycle() {
             unsigned short x = (opcode & 0x0F00) >> 8;
             unsigned short n = (opcode & 0x00FF);
             v[x] = random & n;
+            pc+=2;
             break;
         }
-        case 0xD000:
+        case 0xD000: {
+            unsigned short x = (0x0F00 & opcode) >> 8;
+            unsigned short y = (0x00F0 & opcode) >> 4;
+            unsigned short height = (0x000F & opcode);
+            unsigned short pixel;
+            v[15]=0;
+            for (int yline = 0; yline < height; yline++){
+                pixel = memory[I + yline];
+                for(int xline = 0; xline < 8; xline++){
+                    if((pixel & (0x80 >> xline)) != 0) {
+                        if(gfx[(v[x] + xline + ((v[y] + yline) * 64))] == 1) {
+                            v[15] = 1;
+                        }
+                        gfx[v[x] + xline + ((v[y] + yline) * 64)] ^= 1;
+                    }
+                }
+            }
+            drawFlag = true;
+            pc+=2;
             break;
+        }
         case 0xE000:
             switch (opcode & 0x000F){
                 case 0x0001: {
@@ -244,6 +329,7 @@ void emulateCycle() {
                 case 0x0007: {
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     v[x] = delay_timer;
+                    pc+=2;
                     break;
                 }
                 case 0x000A:{
@@ -261,16 +347,19 @@ void emulateCycle() {
                         }
                     }
                     v[x]=1;
+                    pc+=2;
                     break;
                 }
                 case 0x0015:{
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     delay_timer = v[x];
+                    pc+=2;
                     break;
                 }
                 case 0x0018:{
                     unsigned short x = (0x0F00 & opcode) >> 8;
                     sound_timer = v[x];
+                    pc+=2;
                     break;
                 }
                 case 0x001E:{
@@ -281,17 +370,23 @@ void emulateCycle() {
                         v[15]=0;
                     }
                     I = I + v[x];
+                    pc+=2;
                     break;
                 }
-                case 0x0029:
+                case 0x0029: //TODO
                     break;
-                case 0x0033:
-                    break;
+                case 0x0033:{
+                    memory[I]     = v[(opcode & 0x0F00) >> 8] / 100;
+                    memory[I + 1] = (v[(opcode & 0x0F00) >> 8] / 10) % 10;
+                    memory[I + 2] = (v[(opcode & 0x0F00) >> 8] % 100) % 10;
+                    pc += 2;
+                }
                 case 0x0055: {
                     unsigned short r = (0x0F00 * opcode) >> 8;
                     for (int i = 0; i <= r; ++i) {
                         memory[I+i]=v[i];
                     }
+                    pc+=2;
                     break;
                 }
                 case 0x0065: {
@@ -299,6 +394,7 @@ void emulateCycle() {
                     for (int i = 0; i <= r; ++i) {
                         v[i] = memory[I+i];
                     }
+                    pc+=2;
                     break;
                 }
                 default:
@@ -311,6 +407,7 @@ void emulateCycle() {
 }
 
 int main() {
+    loadgame("pong.rom");
     std::cout << "Hello, World!" << std::endl;
     initialize();
     return 0;
